5e3ef4f65d2df1a713a32caae305a84aee9d03ca
ridget <tom.ridge@tworedkites.com>
2015-02-07 20:44:56 +1000

    created biller model

---
+++ inv_rails/db/migrate/20150207104428_create_billers.rb
@@ -0,0 +1,11 @@
+class CreateBillers < ActiveRecord::Migration
+  def change
+    create_table :billers do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+
+      t.timestamps null: false
+    end
+  end
+end
